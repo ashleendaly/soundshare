1a350982ce768f931df70256018e866daedfbcaf
Sakurazc <2584876z@student.gla.ac.uk>
2022-03-07 05:29:26 +0000

    some basic template

--- soundshare/urls.py
+++ soundshare/urls.py
@@ -14,5 +14,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('', views.index, name='index'),
+                  path('login/', views.login, name='login'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- soundshare/views.py
+++ soundshare/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("SoundShare - The Web App to Share and Rate Music!")
 
-# Create your views here.
+def index(request):
+    context_dict = {'boldmessage': "SoundShare - The Web App to Share and Rate Music!"}
+    return render(request, 'soundshare/index.html', context=context_dict)
+
+
+def login(request):
+    context_dict = {'boldmessage': "SoundShare - The Web App to Share and Rate Music!"}
+    return render(request,'soundshare/login.html',context=context_dict)
+
