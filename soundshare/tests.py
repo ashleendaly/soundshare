264d501347c97bae4daa9edb28d520f392332fb7
Yuan Kuang <2672762k@student.gla.ac.uk>
2022-03-24 04:17:23 +0000

    test: changes about reverse

--- soundshare/tests.py
+++ soundshare/tests.py
@@ -12,7 +12,7 @@ class SillyTestCase(TestCase):
 class IndexViewTests(TestCase):
     def test_index_view_with_no_categories(self):
         # If no categories exist, the appropriate message should be displayed. #
-        response = self.client.get(reverse('soundshare:index'))
+        response = self.client.get('/soundshare/index/')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'There are no categories present.')
         self.assertQuerysetEqual(response.context['song'],[])
@@ -35,7 +35,7 @@ def add_category(name, views=0, likes=0):
     return album
 
 def test_redirect_if_not_logged_in(self):
-    response = self.client.get(reverse('soundshare:index'))
+    response = self.client.get('/soundshare/index/')
     # check redirect #
     self.assertEqual(response.status_code, 302)
     self.assertTrue(response.url.startswith('/soundshare/login/'))
@@ -44,12 +44,12 @@ def test_redirect_if_not_logged_in(self):
 class CategoryViewTestCase(TestCase):
     def setUp(self):
         super().setUp()
-        self.url = reverse('soundshare:index')
-        self.url2 = reverse('soundshare:index')
+        self.url = '/soundshare/index/'
+        self.url2 = '/soundshare/index/'
 
 # Accessing a non-existing category ,expecting a 404 response code #
     def test_visit_a_nonexistent_category(self):
-        url = reverse('soundshare:index')
+        url = '/soundshare/index/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
@@ -58,7 +58,7 @@ class CategoryViewTestCase(TestCase):
     def test_without_any_post(self):
         response = self.client.get(self.url2)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed('soundshare/index.html')
+        self.assertTemplateUsed('/soundshare/index/')
         self.assertContains(response, 'Nothing published for now!')
 
 # Test if the view number is added when it is viewed #
